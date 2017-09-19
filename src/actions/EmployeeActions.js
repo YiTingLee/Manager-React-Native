a89caee98ae6ef71224c8fb8789a36f800fa813a
yitinglee <ranye0719@gmail.com>
2017-09-19 20:57:52 +0800

    able to text message to employees

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -3,7 +3,8 @@ import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE,
   EMPLOYEE_CREATE,
-  EMPLOYEES_FETCH_SUCCESS
+  EMPLOYEES_FETCH_SUCCESS,
+  EMPLOYEES_SAVE_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -39,12 +40,17 @@ export const employeeFetch = () => {
   };
 };
 
-export const employeeSave = ({ name, phone, shift, uid}) => {
+export const employeeSave = ({ name, phone, shift, uid }) => {
   const { currentUser } = firebase.auth();
 
   return (dispatch) => {
     firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
       .set({ name, phone, shift })
-      .then();
+      .then(() => {
+        dispatch({
+          type: EMPLOYEES_SAVE_SUCCESS
+        });
+        Actions.main({ type: 'reset' });
+      });
   };
 };
